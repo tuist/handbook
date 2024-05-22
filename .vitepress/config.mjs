d0dab53bee519526f6623c83177d8bf60aa5dff1
Pedro <pedro@pepicrft.me>
2024-05-22 08:22:40 +0200

    Some adjustments

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -3,26 +3,56 @@ import { defineConfig } from 'vitepress'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   title: "Tuist Handbook",
+  titleTemplate: ":title | Tuist Handbook",
   description: "Tuist company handbook",
+  sitemap: {
+    hostname: "https://handbook.tuist.io",
+  },
+  head: [
+    [
+      "script",
+      {},
+      `
+      !function(t,e){var o,n,p,r;e.__SV||(window.posthog=e,e._i=[],e.init=function(i,s,a){function g(t,e){var o=e.split(".");2==o.length&&(t=t[o[0]],e=o[1]),t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}}(p=t.createElement("script")).type="text/javascript",p.async=!0,p.src=s.api_host.replace(".i.posthog.com","-assets.i.posthog.com")+"/static/array.js",(r=t.getElementsByTagName("script")[0]).parentNode.insertBefore(p,r);var u=e;for(void 0!==a?u=e[a]=[]:a="posthog",u.people=u.people||[],u.toString=function(t){var e="posthog";return"posthog"!==a&&(e+="."+a),t||(e+=" (stub)"),e},u.people.toString=function(){return u.toString(1)+".people (stub)"},o="capture identify alias people.set people.set_once set_config register register_once unregister opt_out_capturing has_opted_out_capturing opt_in_capturing reset isFeatureEnabled onFeatureFlags getFeatureFlag getFeatureFlagPayload reloadFeatureFlags group updateEarlyAccessFeatureEnrollment getEarlyAccessFeatures getActiveMatchingSurveys getSurveys onSessionId".split(" "),n=0;n<o.length;n++)g(u,o[n]);e._i.push([i,s,a])},e.__SV=1)}(document,window.posthog||[]);
+      posthog.init('phc_7yXHIZdYVfi66zcfV6RQzvCYsHneMcNVtjdbM4EurYg',{api_host:'https://eu.i.posthog.com'})
+    `,
+    ],
+    [
+      "script",
+      {},
+      `
+      !function(t){if(window.ko)return;window.ko=[],["identify","track","removeListeners","open","on","off","qualify","ready"].forEach(function(t){ko[t]=function(){var n=[].slice.call(arguments);return n.unshift(t),ko.push(n),ko}});var n=document.createElement("script");n.async=!0,n.setAttribute("src","https://cdn.getkoala.com/v1/pk_3f80a3529ec2914b714a3f740d10b12642b9/sdk.js"),(document.body || document.head).appendChild(n)}();
+    `,
+    ],
+  ],
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
+    logo: "/logo.png",
+    search: {
+      provider: "local",
+    },
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
       {
-        text: 'Examples',
+        text: 'Company',
         items: [
-          { text: 'Markdown Examples', link: '/markdown-examples' },
-          { text: 'Runtime API Examples', link: '/api-examples' }
+          { text: 'Handbook', link: '/' },
+        ],
+      },
+      {
+        text: "Engineering",
+        items: [
+          {text: "Technologies", link: 'handbook/engineering/technologies'},
         ]
       }
     ],
 
     socialLinks: [
-      { icon: 'github', link: 'https://github.com/vuejs/vitepress' }
+      { icon: 'github', link: 'https://github.com/tuist' },
+      { icon: 'slack', link: 'https://join.slack.com/t/tuistapp/shared_invite/zt-1y667mjbk-s2LTRX1YByb9EIITjdLcLw' },
+      { icon: 'mastodon', link: 'https://fosstodon.org/@tuist'}
     ]
   }
 })
--- index.md
+++ index.md
@@ -1,25 +1,11 @@
 ---
-# https://vitepress.dev/reference/default-theme-home-page
-layout: home
-
-hero:
-  name: "Tuist Handbook"
-  text: "Tuist company handbook"
-  tagline: My great project tagline
-  actions:
-    - theme: brand
-      text: Markdown Examples
-      link: /markdown-examples
-    - theme: alt
-      text: API Examples
-      link: /api-examples
-
-features:
-  - title: Feature A
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature B
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
-  - title: Feature C
-    details: Lorem ipsum dolor sit amet, consectetur adipiscing elit
+title: Handbook
 ---
 
+# Handbook
+
+At Tuist, we have learned from the [open-source](https://opensource.org/) community that openness is crucial for creating efficient, asynchronous organizations that thrive in the long term. Inspired by pioneering companies like GitLab, we have embedded openness into the very DNA of our company.
+
+This handbook, alongside our [open-source projects](https://github.com/tuist), embodies our commitment to this value. Whether you are part of Tuist or simply interested in how we operate, this handbook offers insight into our company’s inner workings. It is a living document that will evolve as we grow and learn.
+
+While the future shape of our company remains uncertain, one thing is clear: **we aspire to be remembered for our legacy of remarkable open-source contributions and the transparency that has guided our decisions and defined our company culture.**
